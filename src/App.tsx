0493a95b2f4f4dc78368024b262a8fbd93a47137
Max Vigueras Tovar <mvt.2000@hotmail.com>
2024-10-17 14:21:27 -0600

    Drawer: Drawer & Drawer Navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native"
-import { Text } from "react-native"
 import 'react-native-gesture-handler';
 import { StackNavigator } from "./presentation/routes/StackNavigator";
+import { SideMenuNavigator } from "./presentation/routes/SideMenuNavigator";
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      {/* <StackNavigator/> */}
+      <SideMenuNavigator/>
     </NavigationContainer>
   )
 }
